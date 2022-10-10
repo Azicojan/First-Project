28c2d1fe72cfe57304642db3b7e59316eb684c19
ЗулфикоровА <tokzor123>
2022-10-10 17:33:25 +0500

    added the first task

---
+++ Script.js
@@ -0,0 +1,28 @@
+console.log('Hello world');
+//the task one
+
+/*let stringInfo = function(str){
+    let arr = str.split(' ');
+    let str2 = arr.join('');
+    console.log(arr);
+    console.log(str2);
+
+
+    let countMarks = 0;
+    let countNumbers = 0;
+
+     for(let elem of str2){
+        if (elem === ',' || elem === '.' || elem === '?' || elem === '!' || elem === ';' || elem === '-'){
+            countMarks++;//Here I take the most common punctuation marks. The rest can be added if needed.
+        }
+        if(+(elem) ) {
+            countNumbers++;
+        }
+    }
+    let countChar = str2.length - countMarks - countNumbers;
+    console.log(`Overall,there are ${str2.length} elements in this string:`);
+    console.log(` ${countChar} characters;`)
+    console.log(` ${countMarks} punctuation marks;`);
+    console.log(` and ${countNumbers} digits.`);
+}
+stringInfo('There were 2345 people participating in this event;-,?!;.');*/
