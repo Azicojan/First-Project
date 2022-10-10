d5492b88b9230617208726dc50e362979b5dce8e
ЗулфикоровА <tokzor123>
2022-10-10 17:53:05 +0500

    added the tenth task

--- Script.js
+++ Script.js
@@ -1,20 +1,24 @@
 console.log('Hello world');
-//the task nine
+//the tenth task
 
-/*function separately(str, separator){
+/*let paramIndex = function(template){
 
 
-  for(let key of str){
-    if(key === separator){
+    for(let key of template){
 
-       console.log(str.substring(0,str.indexOf(key)));
-       str = str.substring(str.indexOf(key)+1);
 
+      if(key === '%'){
+
+        for(let i = 1; i < arguments.length; i++){
+
+        template =  template.replace(template[template.indexOf(key)+1],'');
+        template = template.substring(0,key) + template.replace(key,arguments[i]);
+
+      }
+      console.log(template);
+      break;
     }
-
   }
-  return str;
 
 }
-//console.log(separately('10/08/2020/3256/78/9651/104','/'));
-console.log(separately('10/08/2020','/'));*/
+paramIndex('Today is %1 %2.%3.%4, %5','Monday','10','8','2020','15:45.');*/
